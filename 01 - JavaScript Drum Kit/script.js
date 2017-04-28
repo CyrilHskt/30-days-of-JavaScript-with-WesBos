b4ef9c79f09be011919c95a7d69dda6463b2b0d2
Cyril Hasquette <cyril.hasquette@itelios.com>
2017-04-28 11:08:35 +0200

    DrumKit done

--- 01 - JavaScript Drum Kit/script.js
+++ 01 - JavaScript Drum Kit/script.js
@@ -1,6 +1,18 @@
-window.addEventListener('keydown', (e) => {
+function playSound(e) {
     const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`)
+    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`)
     if(!audio) return // stop the function
     audio.currentTime = 0 // rewind to the start
     audio.play();
-})
+    key.classList.add('playing')
+}
+
+function removeTransition(e){
+    if(e.propertyName !== 'transform') return
+    this.classList.remove('playing')
+}
+
+const keys = document.querySelectorAll('.key')
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+
+window.addEventListener('keydown', playSound)
