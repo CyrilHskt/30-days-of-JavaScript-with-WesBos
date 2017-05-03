af0f40aba4111eec4f2958c4a62822f433c8eb69
Cyril Hasquette <cyril.hasquette@itelios.com>
2017-05-03 12:14:19 +0200

    Adding package.json and conf jsHint

--- 06 - Type Ahead/script.js
+++ 06 - Type Ahead/script.js
@@ -9,8 +9,8 @@ fetch(endpoint)
 function findMatches(wordToMatch, cities) {
     return cities.filter(place => {
         const regex = new RegExp(wordToMatch, 'gi');
-        return place.city.match(regex) || place.state.match(regex)
-    })
+        return place.city.match(regex) || place.state.match(regex);
+    });
 }
 
 function displayMatches() {
