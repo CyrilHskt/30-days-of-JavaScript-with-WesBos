c7071bc0ec9cb3794a2a046625a7fa2c777043d7
Cyril Hasquette <cyril.hasquette@itelios.com>
2017-05-03 10:44:18 +0200

    test vscode

---
+++ 06 - Type Ahead/script.js
@@ -0,0 +1,28 @@
+const endpoint = 'https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json';
+
+let cities = [];
+
+fetch(endpoint)
+    .then(blob => blob.json())
+    .then(data => cities.push(...data));
+
+function findMatches(wordToMatch, cities) {
+    return cities.filter(place => {
+        const regex = new RegExp(wordToMatch, 'gi');
+        return place.city.match(regex) || place.state.match(regex)
+    })
+}
+
+console.log('test');
+
+jyy
+
+
+function displayMatches() {
+
+}
+
+const searchInput = document.querySelector('.search');
+const suggestions = document.querySelector('.suggestions');
+
+searchInput.addEventListener('change', displayMatches);
