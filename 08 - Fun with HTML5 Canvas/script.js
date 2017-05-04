330fac98be277277b238e1b0d475d694e81586b8
CyrilHskt <c.hasquette@gmail.com>
2017-05-04 16:38:16 +0200

    canvas done

--- 08 - Fun with HTML5 Canvas/script.js
+++ 08 - Fun with HTML5 Canvas/script.js
@@ -4,4 +4,39 @@ canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 ctx.strokeStyle = '#BADA55';
 ctx.lineJoin = 'round';
-ctx.linecap = 'round';
+ctx.lineCap = 'round';
+
+let isDrawing = false;
+let lastX = 0;
+let lastY = 0;
+let hue = 0;
+let direction = true;
+
+function draw(e) {
+    if(!isDrawing) return;
+    console.log(e);
+    ctx.strokeStyle = `hsl(${hue}, 100%, 50%`;
+    ctx.beginPath();
+    //star from
+    ctx.moveTo(lastX, lastY);
+    // Go to
+    ctx.lineTo(e.offsetX, e.offsetY);
+    ctx.stroke();
+    [lastX, lastY] = [e.offsetX, e.offsetY];
+
+    hue++;
+    if(hue >= 360) hue =0;
+    if(ctx.lineWidth >= 80 || ctx.lineWidth <= 1){
+        direction = !direction;
+    }
+
+    (direction === true) ? ctx.lineWidth++ : ctx.lineWidth--;
+}
+
+canvas.addEventListener('mousemove',draw);
+canvas.addEventListener('mousedown',(e) => {
+    isDrawing = true;
+    [lastX, lastY] = [e.offsetX, e.offsetY];
+});
+canvas.addEventListener('mouseup',() => isDrawing = false);
+canvas.addEventListener('mouseout',() => isDrawing = false);
